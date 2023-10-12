3794293f4f52341dac2005ae6791f278da452529
nguyentrong23 <nguyentrongnguyen10ca2@gmail.com>
2023-10-13 00:48:11 +0700

    13/10/23

---
+++ detect_shape.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+# su dung phuong phap matching template de xac dinh shape can tim
+# doc anh va template
+sr0 = cv2.imread("data/data_shape.png")
+img_src = cv2.cvtColor(sr0,cv2.COLOR_BGR2GRAY)
+
+sr1= cv2.imread("data/template.png")
+img_template = cv2.cvtColor(sr1,cv2.COLOR_BGR2GRAY)
+# list method co the dung khi matching template eval(cv2.TM_CCOEFF),eval(cv2.TM_CCORR_NORMED),eval(cv2.TM_CCORR),eval(cv2.TM_SQDIFF),cv2.TM_SQDIFF_NORMED
+method = eval("cv2.TM_CCOEFF")
+h, w = img_template.shape[::]
+res = cv2.matchTemplate(img_src, img_template, method)
+
+
+# xác dịnh tọa độ và vẽ khung cho template trên ảnh
+minval,maxval,minloc,maxloc = cv2.minMaxLoc(res)
+
+if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+    topleft = minloc
+else:
+    topleft = maxloc
+#     de ve hinh chu nhat thi can biet toa do 2 goc cheo
+bottomright= (topleft[0]+w,topleft[1]+h)
+cv2.rectangle(sr0,topleft,bottomright,(0,255,255),1)
+
+
+#  su dung phung phap counter contours de xac dinh shape can tim
+# sr = cv.imread("data/data_shape.png",1)
+# img = cv.cvtColor(sr,cv.COLOR_BGR2GRAY)
+#
+# _, threshold =cv.threshold(img,80,180, cv.THRESH_BINARY)
+# contours,_ = cv.findContours(threshold,cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# for cnt in contours:
+#     approx=cv.approxPolyDP(cnt, 0.01*cv.arcLength(cnt, True),True)
+#     cv.drawContours(sr,[approx],-1,(0,250,0),1)
+#     x = approx.ravel()[0]
+#     y=  approx.ravel()[1]
+#     if len(approx)==5:
+#         cv.putText(sr,"ngugiac",(x,y),cv.FONT_HERSHEY_COMPLEX,1,(0,255,255),1,cv.LINE_AA)
+#         cv.drawContours(sr,[approx],-1,(255,250,255),2)
+#     if len(approx)==10:
+#         cv.putText(sr,"sao 5 canh",(x,y),cv.FONT_HERSHEY_COMPLEX,1,(0,255,255),1,cv.LINE_AA)
+#         cv.drawContours(sr,[approx],-1,(255,250,0),2)
+
+cv2.imshow("hinh dang",sr0)
+cv2.waitKey(0)
+cv2. destroyAllWindows
---
+++ object_detect_arduino.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import serial
+import cv2
+import time
+
+
+webcam = cv2.VideoCapture(0)
+arduino = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
+global value
+value = 0
+def write_data(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.06)
+    data = arduino.readline()
+    return data
+
+image = cv2.imread("example_opencv/media/balloon.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)#chuyen ve anh xam
+ret, thresh = cv2.threshold(gray, 230,255,cv2.THRESH_BINARY)  #dua ve anh nhi phan tu anh xam theo nguong
+# cv2.imshow("B", thresh)  # hien thi test anh nhi phan
+
+contours, _ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE) # tim contours tu anh nhi phan
+#
+AREA = image.shape[0]*image.shape[1]/150 # kich thuoc toi thieu de duoc ve contours
+dem = 0
+for cnt in contours[:-1]:
+    if cv2.contourArea(cnt) >= AREA:
+        dem+=1
+        cv2.drawContours(image, [cnt], -1, (0,255,0), 2, cv2.LINE_AA)
+# cv2.drawContours(image, contours,-1, (0,255,0),2)# vẽ lại ảnh contour vào ảnh gốc 
+print(dem)
+cv2.imshow("A", image)
+cv2.waitKey()
+cv2.destroyAllWindows()
